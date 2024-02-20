b126cd7de432f1328bc9e51d8b383f2a5c787449
mehadishakil <mehadishakil469@gmail.com>
2024-02-20 23:04:39 +0600

    Worked on adding product to cart

--- app/src/main/java/com/example/petopia/view/ViewProduct.kt
+++ app/src/main/java/com/example/petopia/view/ViewProduct.kt
@@ -2,6 +2,7 @@ package com.example.petopia.view
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.ImageButton
 import android.widget.RatingBar
 import android.widget.TextView
@@ -10,12 +11,17 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.room.Room.databaseBuilder
 import com.denzcoskun.imageslider.constants.ScaleTypes
 import com.denzcoskun.imageslider.models.SlideModel
 import com.example.petopia.R
+import com.example.petopia.db.AppDatabase
+import com.example.petopia.db.Item
+import com.example.petopia.db.ItemDAO
 import com.example.petopia.model.pojo.Product
 
 class ViewProduct : AppCompatActivity() {
+
 
     private lateinit var productImageSlider: com.denzcoskun.imageslider.ImageSlider
     private lateinit var titleTextView: TextView
@@ -26,7 +32,10 @@ class ViewProduct : AppCompatActivity() {
     private lateinit var plusButton: ImageButton
     private lateinit var priceTextView: TextView
     private lateinit var descriptionTextView: TextView
-    private var productPrice: Int? = null
+    private lateinit var addToCart: Button
+    private var productQuantity: Int? = null
+    private var productPrice: String? = null
+    private var productTitle: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -46,6 +55,7 @@ class ViewProduct : AppCompatActivity() {
         quantityTextView = findViewById(R.id.id_quantity)
         plusButton = findViewById(R.id.btn_plus)
         priceTextView = findViewById(R.id.id_price)
+        addToCart = findViewById(R.id.btn_add_to_cart)
         descriptionTextView = findViewById(R.id.tvProductDescription)
 
 
@@ -53,7 +63,10 @@ class ViewProduct : AppCompatActivity() {
         if (intent != null) {
             val currentProduct = intent.getSerializableExtra("object") as Product?
             if (currentProduct != null) {
-                productPrice = currentProduct.price.toInt()
+                productPrice = currentProduct.price
+                productTitle = currentProduct.title
+
+
                 titleTextView.text = currentProduct.title
                 priceTextView.text = currentProduct.price+" tk"
                 descriptionTextView.text = currentProduct.description
@@ -101,6 +114,38 @@ class ViewProduct : AppCompatActivity() {
                 val yourPrice: Int = value * productPrice!!
                 priceTextView.setText(yourPrice.toString())
                 quantityTextView.setText(value.toString())
+            }
+        })
+
+
+
+
+
+        addToCart.setOnClickListener(View.OnClickListener {
+            try {
+                productQuantity = quantityTextView.getText().toString().toInt()
+                val id = Math.random().toInt()
+
+                val db: AppDatabase = databaseBuilder(
+                    applicationContext,
+                    AppDatabase::class.java, "cart_db"
+                ).allowMainThreadQueries().build()
+                val itemDAO: ItemDAO = db.itemDAO()
+                val check: Boolean = itemDAO.is_exist(productTitle)
+                if (check == false) {
+                    try {
+                        itemDAO.insertRecord(Item(id, productTitle, productPrice, productQuantity.toString()))
+                        Toast.makeText(this@ItemView, "Item added to cart", Toast.LENGTH_SHORT)
+                            .show()
+                    } catch (e: Exception) {
+                        Toast.makeText(this@ItemView, "" + e, Toast.LENGTH_SHORT).show()
+                    }
+                } else {
+                    Toast.makeText(this@ItemView, "Item Already exist in cart", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            } catch (e: Exception) {
+                Toast.makeText(this@ItemView, "" + e, Toast.LENGTH_SHORT).show()
             }
         })
 
